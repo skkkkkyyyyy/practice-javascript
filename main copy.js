3d30874d7ecb788791a3ea45a890ddd27e12c9fb
skkkkkyyyyy <sora.oyama.0609@icloud.com>
2024-09-19 05:18:25 +0000

    9_19 JavaScriptの導入

---
+++ main copy.js
@@ -0,0 +1,10 @@
+document.querySelector("h1").textContent = "おやまそら"
+
+//documentとはHTMLファイルの書かれているコードを一つのオブジェクトとしてみなす。
+
+//dom  ドキュメントオブジェクトモデルの略で、webのページの要素やコンテンツなどをツリー構造で表現したデータモデル
+
+//document.querySelector("h1").textContent = "おやまそら"の意味
+//document.querySelector("h1")
+//h1タグをさがしてね
+//h1タグの中身のテキストを変えてね
