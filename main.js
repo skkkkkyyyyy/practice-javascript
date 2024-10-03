e4f8e54f3ed26e83c43f46685349bfadbd988d46
skkkkkyyyyy <sora.oyama.0609@icloud.com>
2024-10-03 05:20:35 +0000

    10_3

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
-function changeColor(){
-    document.getElementById("text").style.color = "red";
-}
-//document.scrollingElementById("text").style.color = "red";
-//これはドキュメントの中の「text」というIDをもつ
-//タグのスタイルの色を赤にしてください
+//クリックされたら動作する
+function errorInput(){
+    console.log(document.getElementById("month").value)
+    console.log( document.getElementById("day").value)
+   
+    const month = document.getElementById("month").value;
+    const month = document.getElementById("day").value;
 
-//[.]は～のという意味
-
-
-let count = 0;
-
-function increaseCount(){
-
-count++;
-document.getElementById("counter").innerText = count;
+    console.log("month:",month,"day:",day)
 
 }
 
-function herasuCount(){
-count--;
-document.getElementById("counter").innerText = count;
-
-}
+//javascriptのプログラムとHTMLはどこで対応しているか
+//タグについているIDで紐づく
+//タグの中にある負荷情報のことを属性という
